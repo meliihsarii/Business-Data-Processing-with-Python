9eeb8fd518ac279929762d6096842c7b0173a50d
meliihsarii <55879188+meliihsarii@users.noreply.github.com>
2019-10-16 01:25:22 +0300

    Add files via upload

---
+++ BA4318 Homework 3.py
@@ -0,0 +1,18 @@
+age = float(input("Enter your age:"))
+ 
+if (age >=20):
+    
+    print("Adult")
+
+elif(10<=age<=19):
+        
+    print("Adolescent")
+    
+elif (1<=age<=9):
+    
+            print("Child")
+elif (age<1):
+    
+            print("Infant")
+    
+#I defined float function since infants have age as monthly, it provided me to recognize their age after transformed a float value
