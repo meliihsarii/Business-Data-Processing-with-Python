58b87b34b20e7912258fec0dc4bec867876f3e39
meliihsarii <55879188+meliihsarii@users.noreply.github.com>
2019-10-16 01:22:07 +0300

    Add files via upload

---
+++ BA4318 Homework 3.py
@@ -0,0 +1,17 @@
+age = float(input("Enter your age:"))
+ 
+if (age >=20):
+    
+    print("Adult")
+
+elif(10<=age<=19):
+        
+    print("Adolescent")
+    
+elif (1<=age<=9):
+    
+            print("Child")
+elif (age<1):
+    
+            print("Infant")
+    
